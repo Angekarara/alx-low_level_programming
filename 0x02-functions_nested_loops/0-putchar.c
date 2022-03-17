33590f54173deb424ac4b4d9ebef0b1322a3628d
Angekarara <mbabange2020@gmail.com>
2022-03-17 15:43:09 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
-#include "main.h" 
+#include "main.h"
 /**
- * main - Prints _putchar as a message.
- * characters printed
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,5 +14,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
+
